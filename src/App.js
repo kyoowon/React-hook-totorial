1d07703561c35f96b3b2ec9cab527bf7f7d2b650
kyoowon <kyoowon95@gmail.com>
2022-04-12 23:38:25 +0900

    add(*) : initial files for totorial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Example, Example_class} from './Components/Example_1.js';
+
+function App() {
+	return (
+		<div className="App">
+            {/* react hooks - useState */}
+			<Example></Example>
+			<Example_class></Example_class>
+		</div>
+	);
+}
+
+export default App;
