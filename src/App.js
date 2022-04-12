b7162d83f55fa38bf4076e8a148966d393ab72dd
kyoowon <kyoowon95@gmail.com>
2022-04-12 23:58:10 +0900

    fix(1): 2 space to be the uniformity of editors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {Example, Example_class} from './Components/Example_1.js';
 
 function App() {
-	return (
-		<div className="App">
-            {/* react hooks - useState */}
-			<Example></Example>
-			<Example_class></Example_class>
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* react hooks - useState */}
+      <Example></Example>
+      <Example_class></Example_class>
+    </div>
+  );
 }
 
 export default App;
