afedecdde670be5ea65759ddaf5f3ecd5029a0e4
kyoowon <kyoowon95@gmail.com>
2022-04-14 22:25:54 +0900

    ✅ update(useEffect) : tab to space

--- src/Components/useEffect.js
+++ src/Components/useEffect.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
 export function Example_4() {
-    const seyHello = () => console.log("hello");
-    const [number, setNumber] = useState(0);
-    const [anumber, setAnumber] = useState(0);
-    // []안에 number, anumber 등을 넣어 해당 값이 변경될 때 마다 이벤트가 발생하도록 하는 메서드
-    useEffect(seyHello, [number])
-    return (
-        <div>
-            <h1>useEffect</h1>
-            <button onClick={() => setNumber(number + 1)}>{number}</button>
-            <button onClick={() => setAnumber(anumber + 1)}>{anumber}</button>
-        </div>
-    )
+  const seyHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [anumber, setAnumber] = useState(0);
+  // []안에 number, anumber 등을 넣어 해당 값이 변경될 때 마다 이벤트가 발생하도록 하는 메서드
+  useEffect(seyHello, [number])
+  return (
+    <div>
+      <h1>useEffect</h1>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(anumber + 1)}>{anumber}</button>
+    </div>
+  )
 }
